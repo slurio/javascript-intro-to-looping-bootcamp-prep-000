f9beba90b743ecb9efec5e26e69362423fbe8201
Samantha Lurio <samantha.lurio@gmail.com>
2020-06-13 02:50:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for(let i = 0; i<25; i++) {
-    if(i===0) {
+    if(i===1) {
       array.push("I am one strange loop.")
     } else {
       array.push(`I am ${i} strange loops.`)
